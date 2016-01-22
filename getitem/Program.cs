1cf1075c355c7905fe1eb092c297e7275abac2ea
DarkSpirit444 <dspirit@gmail.com>
2016-01-21 17:52:56 -0800

    added additional info to display

--- getitem/Program.cs
+++ getitem/Program.cs
@@ -108,136 +108,142 @@ namespace getitem
         static void Main(string[] args)
         {
             string itemName;
-            if (args.Count() == 0)
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+
+            do
             {
-                Console.WriteLine("Enter item name: ");
-                itemName = Console.ReadLine();
-            }
-            else itemName = args[0];
-
-            try
-            {
-                BLSalvageCost = trader.BlackLionKitSalvageCost;
-                Console.WriteLine("BL Salvage Cost: {0}", BLSalvageCost);
-
-                List<Item> itemList = trader.search_items(itemName, false).Result;
-
-                if (itemList.Count > 0)
+                if (args.Count() == 0)
                 {
-                    uint count = 0;
-                    foreach (Item item in itemList)
-                    {
-                        if (String.Compare(itemName, item.Name, true) != 0) continue;
-
-                        Item richItem = trader.make_rich_item(item).Result;
-                        int margin = richItem.MinSaleUnitPrice - richItem.MaxOfferUnitPrice;
-                        double tpSalePrice = richItem.MaxOfferUnitPrice * 0.85;
-                        richItem.setConsoleColor();
-                        Console.WriteLine("");
-                        Console.WriteLine("{0}) {1}: \"{2}\" TP Sale Profit: {3} Margin: {4}", count, richItem.Id, richItem.Name, tpSalePrice, margin);
-                        Console.WriteLine("Description: {0}", richItem.Description);
-                        Console.WriteLine("Rarity: {0}", richItem.RarityDescription);
-                        Console.WriteLine("Level: {0}", richItem.MinLevel);
-                        Console.WriteLine("Max Offer Unit Price: {0}", richItem.MaxOfferUnitPrice);
-                        Console.WriteLine("Min Sale Unit Price: {0}", richItem.MinSaleUnitPrice);
-                        Console.WriteLine("Offer Availability: {0}", richItem.BuyCount);
-                        Console.WriteLine("Sale Availability: {0}", richItem.SellCount);
-                        Console.WriteLine("Vendor Price: {0}", richItem.VendorPrice);
-
-                        if (richItem.IsRich)
-                        {
-                            Console.WriteLine("Price Last Changed: {0}", richItem.PriceLastChanged);
-                            Console.WriteLine("% Offer Price Change Last Hour: {0}", richItem.OfferPriceChangedLastHour);
-                            Console.WriteLine("% Sale Price Change Last Hour: {0}", richItem.SalePriceChangedLastHour);
-                            Console.WriteLine("Type Id: {0}", richItem.TypeId);
-                            Console.WriteLine("SubType Id: {0}", richItem.SubTypeDescription);
-                            Console.WriteLine("Monthly Offer Average Price: {0}", trader.monthlyOfferAverage(richItem.Id));
-                            Console.WriteLine("Monthly Sell Average Price: {0}", trader.monthlySellAverage(richItem.Id));
-                            Console.WriteLine("If salvaging using Black Lion Salvage Kit...");
-
-                            //Salvage level 68+ rare with MSK = 0.9 ecto
-                            //Salvage level 68+ rare with BLSK = 1.25 ecto
-                            //Salvage level 68+ exotic with MSK = 1.27 ecto
-                            //Salvage level 68+ exotic with BLSK = 1.75 ecto
-                            //Salvage exotic that can give inscription/insignia with MSK = 42% chance of getting inscription/insignia
-                            //Salvage exotic that can give inscription/insignia with BLSK = 61% chance of getting inscription/insignia
-
-                            double ectoChanceBLSK = 0.0, ectoChanceMSK = 0.0;
-                            double insigniaChanceBLSK = 0.0, insigniaChanceMSK = 0.0;
-                            if (richItem.MinLevel >= 68 && richItem.RarityId >= RarityEnum.Rare)
-                            {
-                                ectoChanceBLSK = (richItem.RarityId == RarityEnum.Rare) ? 1.25 : 1.75;
-                                ectoChanceMSK = (richItem.RarityId == RarityEnum.Rare) ? 0.875 : 1.27;
-                            }
-
-                            if (richItem.RarityId >= RarityEnum.Exotic)
-                            {
-                                insigniaChanceBLSK = 0.61;
-                                insigniaChanceMSK = 0.42;
-                            }
-
-                            bool toSalvage;
-                            if (!SalvageOrSell(richItem, BLSalvageCost, out toSalvage, 1.0, ectoChanceBLSK, insigniaChanceBLSK))
-                                Console.WriteLine("More Profitable to sell for {0}", Math.Max(item.MaxOfferUnitPrice * 0.85, item.VendorPrice));
-                            else if (!toSalvage)
-                            {
-                                Console.WriteLine("If salvaging using Master Salvage Kit...");
-                                if (!SalvageOrSell(richItem, trader.MasterKitSalvageCost, out toSalvage, 0.8, ectoChanceMSK, insigniaChanceMSK))
-                                    Console.WriteLine("More Profitable to sell for {0}", Math.Max(item.MaxOfferUnitPrice * 0.85, item.VendorPrice));
-                            }
-                        }
-
-                        Console.ResetColor();
-                        if (richItem.VendorPrice > tpSalePrice)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("More Profitable to sell to the Vendor than to the Trading Post!");
-                            Console.ResetColor();
-                        }
-
-                        count++;
-                    } // end of foreach (Item item in itemList)
-
-                    int choice = 0;
-                    if (count > 1)
-                    {
-                        Console.WriteLine("");
-                        Console.WriteLine("Select which item to view the sell listing :");
-                        while ((choice = ReadKey()) >= count) ;
-                    }
-
-                    if (count > 0)
-                    {
-                        int boughtPrice = trader.GetMyBoughtPrice(itemList[choice], DateTime.Now); // get the most recently bought of such an item
-                        int sellingPrice = GetMySellingPrice(itemList[choice].Id);
-                        int breakEvenPrice = (int)Math.Ceiling(boughtPrice / 0.85);
-                        if (boughtPrice >= 0)
-                        {
-                            Console.WriteLine("Found bought price = {0}.  BreakEven price = {1}", boughtPrice, breakEvenPrice);
-                        }
-
-                        List<ItemBuySellListingItem> itemSellListing = trader.get_sell_listings(itemList[choice].Id).Result;
-                        foreach (ItemBuySellListingItem sellListing in itemSellListing)
-                        {
-                            if (sellListing.PricePerUnit < breakEvenPrice) Console.ForegroundColor = ConsoleColor.Red;
-                            if (sellingPrice >= 0 && sellListing.PricePerUnit == sellingPrice) Console.Write(">>>>> ");
-                            Console.WriteLine("{0} listed by {1} seller(s).  Unit Price: {2}", sellListing.NumberAvailable, sellListing.NumberOfListings, sellListing.PricePerUnit);
-                            Console.ResetColor();
-                        }
-                    }
-                    //else
-                    //    Console.WriteLine("Sorry!  We can't find any {0}.", itemName);
-
-                    Console.WriteLine("Hit ENTER to exit...");
-                    Console.ReadLine();
+                    Console.WriteLine("Enter item name: ");
+                    itemName = Console.ReadLine();
                 }
-                else
-                    Console.WriteLine("Sorry!  We can't find any {0}.", itemName);
+                else itemName = args[0];
+
+                try
+                {
+                    BLSalvageCost = trader.BlackLionKitSalvageCost;
+                    Console.WriteLine("BL Salvage Cost: {0}", BLSalvageCost);
+
+                    List<Item> itemList = trader.search_items(itemName, false).Result;
+
+                    if (itemList.Count > 0)
+                    {
+                        uint count = 0;
+                        foreach (Item item in itemList)
+                        {
+                            if (String.Compare(itemName, item.Name, true) != 0) continue;
+
+                            Item richItem = trader.make_rich_item(item).Result;
+                            int margin = richItem.MinSaleUnitPrice - richItem.MaxOfferUnitPrice;
+                            double tpSalePrice = richItem.MaxOfferUnitPrice * 0.85;
+                            richItem.setConsoleColor();
+                            Console.WriteLine("");
+                            Console.WriteLine("{0}) {1}: \"{2}\" TP Sale Profit: {3} Margin: {4}", count, richItem.Id, richItem.Name, tpSalePrice, margin);
+                            Console.WriteLine("Description: {0}", richItem.Description);
+                            Console.WriteLine("Rarity: {0}", richItem.RarityDescription);
+                            Console.WriteLine("Level: {0}", richItem.MinLevel);
+                            Console.WriteLine("Max Offer Unit Price: {0}", richItem.MaxOfferUnitPrice);
+                            Console.WriteLine("Min Sale Unit Price: {0}", richItem.MinSaleUnitPrice);
+                            Console.WriteLine("Offer Availability: {0}", richItem.BuyCount);
+                            Console.WriteLine("Sale Availability: {0}", richItem.SellCount);
+                            Console.WriteLine("Vendor Price: {0}", richItem.VendorPrice);
+
+                            if (richItem.IsRich)
+                            {
+                                Console.WriteLine("Price Last Changed: {0}", richItem.PriceLastChanged);
+                                Console.WriteLine("% Offer Price Change Last Hour: {0}", richItem.OfferPriceChangedLastHour);
+                                Console.WriteLine("% Sale Price Change Last Hour: {0}", richItem.SalePriceChangedLastHour);
+                                Console.WriteLine("Type Id: {0}", richItem.TypeId);
+                                Console.WriteLine("SubType Id: {0}", richItem.SubTypeDescription);
+                                Console.WriteLine("Monthly Offer Average Price: {0}", trader.monthlyOfferAverage(richItem.Id));
+                                Console.WriteLine("Monthly Sell Average Price: {0}", trader.monthlySellAverage(richItem.Id));
+                                Console.WriteLine("If salvaging using Black Lion Salvage Kit...");
+
+                                //Salvage level 68+ rare with MSK = 0.9 ecto
+                                //Salvage level 68+ rare with BLSK = 1.25 ecto
+                                //Salvage level 68+ exotic with MSK = 1.27 ecto
+                                //Salvage level 68+ exotic with BLSK = 1.75 ecto
+                                //Salvage exotic that can give inscription/insignia with MSK = 42% chance of getting inscription/insignia
+                                //Salvage exotic that can give inscription/insignia with BLSK = 61% chance of getting inscription/insignia
+
+                                double ectoChanceBLSK = 0.0, ectoChanceMSK = 0.0;
+                                double insigniaChanceBLSK = 0.0, insigniaChanceMSK = 0.0;
+                                if (richItem.MinLevel >= 68 && richItem.RarityId >= RarityEnum.Rare)
+                                {
+                                    ectoChanceBLSK = (richItem.RarityId == RarityEnum.Rare) ? 1.25 : 1.75;
+                                    ectoChanceMSK = (richItem.RarityId == RarityEnum.Rare) ? 0.875 : 1.27;
+                                }
+
+                                if (richItem.RarityId >= RarityEnum.Exotic)
+                                {
+                                    insigniaChanceBLSK = 0.61;
+                                    insigniaChanceMSK = 0.42;
+                                }
+
+                                bool toSalvage;
+                                if (!SalvageOrSell(richItem, BLSalvageCost, out toSalvage, 1.0, ectoChanceBLSK, insigniaChanceBLSK))
+                                    Console.WriteLine("More Profitable to sell for {0}", Math.Max(item.MaxOfferUnitPrice * 0.85, item.VendorPrice));
+                                else if (!toSalvage)
+                                {
+                                    Console.WriteLine("If salvaging using Master Salvage Kit...");
+                                    if (!SalvageOrSell(richItem, trader.MasterKitSalvageCost, out toSalvage, 0.8, ectoChanceMSK, insigniaChanceMSK))
+                                        Console.WriteLine("More Profitable to sell for {0}", Math.Max(item.MaxOfferUnitPrice * 0.85, item.VendorPrice));
+                                }
+                            }
+
+                            Console.ResetColor();
+                            if (richItem.VendorPrice > tpSalePrice)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("More Profitable to sell to the Vendor than to the Trading Post!");
+                                Console.ResetColor();
+                            }
+
+                            count++;
+                        } // end of foreach (Item item in itemList)
+
+                        //int choice = 0;
+                        //if (count > 1)
+                        //{
+                        //    Console.WriteLine("");
+                        //    Console.WriteLine("Select which item to view the sell listing :");
+                        //    while ((choice = ReadKey()) >= count) ;
+                        //}
+
+                        //if (count > 0)
+                        //{
+                        //    int boughtPrice = trader.GetMyBoughtPrice(itemList[choice], DateTime.Now); // get the most recently bought of such an item
+                        //    int sellingPrice = GetMySellingPrice(itemList[choice].Id);
+                        //    int breakEvenPrice = (int)Math.Ceiling(boughtPrice / 0.85);
+                        //    if (boughtPrice >= 0)
+                        //    {
+                        //        Console.WriteLine("Found bought price = {0}.  BreakEven price = {1}", boughtPrice, breakEvenPrice);
+                        //    }
+
+                        //    List<ItemBuySellListingItem> itemSellListing = trader.get_sell_listings(itemList[choice].Id).Result;
+                        //    foreach (ItemBuySellListingItem sellListing in itemSellListing)
+                        //    {
+                        //        if (sellListing.PricePerUnit < breakEvenPrice) Console.ForegroundColor = ConsoleColor.Red;
+                        //        if (sellingPrice >= 0 && sellListing.PricePerUnit == sellingPrice) Console.Write(">>>>> ");
+                        //        Console.WriteLine("{0} listed by {1} seller(s).  Unit Price: {2}", sellListing.NumberAvailable, sellListing.NumberOfListings, sellListing.PricePerUnit);
+                        //        Console.ResetColor();
+                        //    }
+                        //}
+                        //else
+                        //    Console.WriteLine("Sorry!  We can't find any {0}.", itemName);
+
+                        Console.WriteLine("Hit ENTER to exit...");
+                        key = Console.ReadKey();
+                    }
+                    else
+                        Console.WriteLine("Sorry!  We can't find any {0}.", itemName);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(ExceptionHelper.FlattenException(e));
+                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(ExceptionHelper.FlattenException(e));
-            }
+            while (key.Key != ConsoleKey.Enter);
         }
     }
 }
